bb4484ec210409d6fe63806de94e3e48759654f2
iamdaviddev <davidpaulodev@gmail.com>
2024-06-18 12:41:05 +0100

    ci: configs added

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 export function Profile () {
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Profile</Text>
     </View>
   )
